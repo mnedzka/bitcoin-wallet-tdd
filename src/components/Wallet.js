9e315086630298197a9637c6ddc8a0ee6d017592
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-12 16:11:26 +0100

    Add possibility to add the balance in the component and test it

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class Wallet extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      balance: undefined
+    };
+  }
+
+  updateBalance = e => {
+    this.setState({ balance: parseInt(e.target.value, 10) });
+  };
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
+        <br />
+        <input className="input-wallet" onChange={this.updateBalance} />
       </div>
     );
   }
