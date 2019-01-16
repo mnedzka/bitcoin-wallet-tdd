6b19a989695eaf615519875325d8d49e7fee5d6d
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-16 22:36:55 +0100

    Fix typo in import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { createStore, applyMiddleware } from "redux";
-import thunk from redux-thunk;
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
 
